562ef37c86d4cab392286d12bb0edaf6664dd55b
felix <felix.rauch@student.uibk.ac.at>
2022-04-28 17:31:31 +0200

    Layer hinzufügen

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 /* OGD Wien Beispiel */
+
+let stephansdom = {
+    lat:48.208493,
+    lng:16.373118,
+    title: "Sephansdom"
+}
+
+let startLayer = L.tileLayer.provider("BasemapAT.basemap")
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+})
+
+let layerControl = L.control.layers({
+    "BasemapAT Grau" : startLayer,
+}).addTo(map)
