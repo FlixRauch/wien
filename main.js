710aa2211553a82d898c62f8cffae71689b16246
felix <felix.rauch@student.uibk.ac.at>
2022-05-12 16:48:24 +0200

    Cluster für Hotels

--- main.js
+++ main.js
@@ -194,7 +194,7 @@ async function loadHotels(url) {
     let geojson = await response.json(url)
     
 
-    let overlay = L.featureGroup();
+    let overlay = L.markerClusterGroup();
 
     layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
     overlay.addTo(map);
